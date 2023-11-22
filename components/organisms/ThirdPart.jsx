973a9304002f1657ce8134f3b02863c85c7a5b7a
chaemina <chaemina82@naver.com>
2023-11-22 13:30:43 +0900

    fix: intro 수정
    intro의 내용을 실제 소개로 수정함

--- components/organisms/ThirdPart.jsx
+++ components/organisms/ThirdPart.jsx
@@ -17,16 +17,18 @@ const ThirdPart = () => {
       <div ref={ref} id="intro" className="w-full space-y-10 opacity-0">
         <Title className="bold">Intro</Title>
         <div className="flex">
-          <p className="w-[40%]">
-            Tenho 20 anos, estou em transição de carreira, vindo da área
-            administrativa onde desenvolvi experiência com gestão de pessoas,
-            gestão de empresas e negociação com cliente. Hoje construo
-            aplicações fullstack!
+          <p className="text-lg w-[45%]">
+            저는 전남대학교 소프트웨어 공학과를 재학 중인 25살 백앤드 개발자
+            입니다. 카카오 주관 카카오 테크 캠퍼스의 최종 발표에서 "축팅" 이라는
+            서비스를 개발하여 우수상을 수상하였습니다. 해당 서비스에서 저는
+            스프링부트와 스프링시큐리티를 활용해서 보안, 상태관리 분야를
+            중점으로 개발했습니다. 상단의 Projects 파트 "축팅"의 깃허브에서 저의
+            코드를 확인 하실 수 있습니다.
           </p>
           <Image
             src={ProfileImage}
             alt="profile Image"
-            className="max-w-[50%] max-h-[50%]"
+            className="max-w-[50%] h-[20rem]"
           ></Image>
         </div>
         <Title className="text-3xl">Skills</Title>
